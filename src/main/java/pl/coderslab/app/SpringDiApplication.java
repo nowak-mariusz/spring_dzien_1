58d1fa73f14f98659cbb4dd2a202c91e947a9027
mnowak <mnowak2@future-processing.com>
2019-05-25 20:24:09 +0200

    zadanie 5: autokonfiguracja, skanowanie w poszukiwaniu adnotacji @Component

--- src/main/java/pl/coderslab/app/SpringDiApplication.java
+++ src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -1,7 +1,17 @@
 package pl.coderslab.app;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pl.coderslab.beans.PersonService;
+
 public class SpringDiApplication {
     public static void main(String[] args) {
 
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(BeanConfiguration.class);
+
+        PersonService person = context.getBean(PersonService.class);
+        System.out.println(person.getPersonRepo().getClass().getName());
+
+        context.close();
     }
 }
