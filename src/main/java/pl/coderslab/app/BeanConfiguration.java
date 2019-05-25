754a1b303582090b8a4312b96198fcd91d56e72a
mnowak <mnowak2@future-processing.com>
2019-05-25 19:58:56 +0200

    zadanie 1: pusty kontekst (AnnotationConfigApplicationContext)

---
+++ src/main/java/pl/coderslab/app/BeanConfiguration.java
@@ -0,0 +1,8 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+}
--- src/main/java/pl/coderslab/app/SpringDiApplication.java
+++ src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -1,7 +1,15 @@
 package pl.coderslab.app;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SpringDiApplication {
     public static void main(String[] args) {
 
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(BeanConfiguration.class);
+
+        // wstaje pusty kontekst
+
+        context.close();
     }
 }
